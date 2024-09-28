4b5c87af2d5e816641a548235416a4c5ffedbfd9
smboon <smboonsg@hotmail.com>
2024-09-28 19:27:58 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/stock-purchases/",
   plugins: [react()],
+  base: "/stock-purchases/",
   test: {
     globals: true,
     environment: "happy-dom",
